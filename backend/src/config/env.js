b27191784347147030ee86dbf5121e977feb3f3f
Shivam <sk3002882@gmail.com>
2026-03-27 15:46:14 +0530

    fix/env

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -4,10 +4,11 @@ dotenv.config();
 // We create a clean object to export
 export const env = {
     port: process.env.PORT || 5000,
-     mongoUri: process.env.MONGODB_URI,
+    mongoUri: process.env.MONGODB_URI,
     upstashRedisUrl: process.env.UPSTASH_REDIS_URL,
     jwtSecret: process.env.JWT_SECRET,
     nodeEnv: process.env.NODE_ENV || 'development',
     accessSecret: process.env.ACCESS_TOKEN_SECRET,
     refreshSecret: process.env.REFRESH_TOKEN_SECRET,
+    frontendUrl: process.env.FRONTEND_URL,
 };
